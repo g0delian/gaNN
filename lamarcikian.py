668f67a714abbf045e9318e513e4e440b36457d2
g0delian <25383450+g0delian@users.noreply.github.com>
2020-12-30 17:51:57 +0000

    test set

--- lamarcikian.py
+++ lamarcikian.py
@@ -14,7 +14,7 @@ num_bytes = 15
 
 
 def fitness(solution, sol_idx):
-    global data_inputs, data_outputs, keras_ga, model
+    global training_inputs, training_outputs, keras_ga, model, testing
 
     weight_list = []
     for i in range(0, len(solution), 15):
@@ -28,10 +28,15 @@ def fitness(solution, sol_idx):
 
     model.set_weights(weights=model_weights_matrix)
 
-    predictions = model.predict(data_inputs)
+    if testing:
+        predictions = model.predict(test_inputs)
+        mae = tensorflow.keras.losses.MeanSquaredError()
+        abs_error = mae(test_outputs, predictions).numpy() + 0.00000001
+    else:
+        predictions = model.predict(training_inputs)
+        mae = tensorflow.keras.losses.MeanSquaredError()
+        abs_error = mae(training_outputs, predictions).numpy() + 0.00000001
 
-    mae = tensorflow.keras.losses.MeanSquaredError()
-    abs_error = mae(data_outputs, predictions).numpy() + 0.00000001
     solution_fitness = 1.0 / abs_error
 
     return solution_fitness
@@ -52,11 +57,12 @@ def lt_learning(solution_idx):
 
     model.set_weights(weights=model_weights_matrix)
     model.compile(optimizer="rmsprop", loss="mse")
-    model.fit(data_inputs, data_outputs, epochs=3) # 30 iterations, 3 epoch
-    predictions = model.predict(data_inputs)
+    model.fit(training_inputs, training_outputs,
+              epochs=3)  # 30 iterations, 3 epoch
+    predictions = model.predict(training_inputs)
     local_search = pygad.kerasga.model_weights_as_vector(model)
     mae = tensorflow.keras.losses.MeanSquaredError()
-    abs_error = mae(data_outputs, predictions).numpy() + 0.00000001
+    abs_error = mae(training_outputs, predictions).numpy() + 0.00000001
     solution_fitness = 1.0 / abs_error
 
     # print(local_search)
@@ -71,18 +77,19 @@ def lt_learning(solution_idx):
 
 
 def callback_generation(ga_instance):
-    global losses
+    global training_loss
     print("Generation = {generation}".format(
         generation=ga_instance.generations_completed))
     print("Fitness    = {fitness}".format(
         fitness=ga_instance.best_solution()[1]))
     print("Loss    = {loss_value}".format(
-        loss_value= 1.0 / ga_instance.best_solution()[1]))
-    losses.append(1.0 / ga_instance.best_solution()[1])
+        loss_value=1.0 / ga_instance.best_solution()[1]))
+    training_loss.append(1.0 / ga_instance.best_solution()[1])
 
 
 def callback_fitness(ga_instance, population_fitness):
     # skip initial pop, start from gen 1
+    global best_individuals
     global initial_population
     if initial_population:
         initial_population = False
@@ -93,8 +100,10 @@ def callback_fitness(ga_instance, population_fitness):
     if evolution_type == "lamarck":
         ga_instance.population[best_solt_idx] = lt_vector
         population_fitness[best_solt_idx] = lt_value
-    if evolution_type=="baldwin":
+    if evolution_type == "baldwin":
         population_fitness[best_solt_idx] = lt_value
+
+    best_individuals.append(ga_instance.population[best_solt_idx])
 
 
 # Neural network has one hidden layer with six neurons.
@@ -108,22 +117,33 @@ model = tensorflow.keras.Model(inputs=input_layer, outputs=output_layer)
 
 keras_ga = pygad.kerasga.KerasGA(model=model, num_solutions=10)
 
-data_inputs = []
-data_outputs = []
-losses = []
+training_inputs = []
+training_outputs = []
+test_inputs = []
+test_outputs = []
+training_loss = []
 
 # read training data from the file
 file_t = open("train.dat", "r")
 lines_t = file_t.readlines()
 for line in lines_t:
     x1, x2, y = line.split()
-    data_inputs.append([float(x1), float(x2)])
-    data_outputs.append([float(y)])
+    training_inputs.append([float(x1), float(x2)])
+    training_outputs.append([float(y)])
 
-num_generations = 25
+file_t = open("test.dat", "r")
+lines_t = file_t.readlines()
+for line in lines_t:
+    x1, x2, y = line.split()
+    test_inputs.append([float(x1), float(x2)])
+    test_outputs.append([float(y)])
+
+num_generations = 5
 num_parents_mating = 2
 evolution_type = "lamarck"  # {lamarck, baldwin}
 initial_population = True
+testing = False
+best_individuals = []
 
 ga_instance = pygad.GA(num_generations=num_generations,
                        num_parents_mating=num_parents_mating,
@@ -148,9 +168,24 @@ ga_instance.plot_result(
 
 # losses of best individuals in every generation
 plt.figure()
-plt.plot(range(len(losses)), losses, linewidth=3)
-plt.title("Generation vs Loss")
+plt.plot(range(len(training_loss)), training_loss, linewidth=3)
+plt.title("TRAINING Generation vs Loss")
 plt.ylabel('Loss')
 plt.xlabel('Generation')
 plt.show()
 
+# best individual in each generation
+testing = True
+test_loss = []
+for individual in best_individuals:
+    loss = 1.0 / fitness(individual, 0)
+    test_loss.append(loss)
+
+print(test_loss)
+
+plt.figure()
+plt.plot(range(len(test_loss)), test_loss, linewidth=3)
+plt.title("TEST Generation vs Loss")
+plt.ylabel('Loss')
+plt.xlabel('Generation')
+plt.show()
